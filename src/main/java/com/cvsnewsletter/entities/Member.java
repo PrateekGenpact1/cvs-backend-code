4c63e094622bbe9819d1115c0ae37759ae8e9fe0
Prateek825 <prateeksingh825@gmail.com>
2025-12-09 15:09:13 +0530

    added prefix in role in jwt token

--- src/main/java/com/cvsnewsletter/entities/Member.java
+++ src/main/java/com/cvsnewsletter/entities/Member.java
@@ -112,7 +112,7 @@ public class Member implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return List.of(new SimpleGrantedAuthority(role.getPrefixedValue()));
     }
 
     @Override
--- src/main/java/com/cvsnewsletter/entities/enums/Role.java
+++ src/main/java/com/cvsnewsletter/entities/enums/Role.java
@@ -2,15 +2,22 @@ package com.cvsnewsletter.entities.enums;
 
 import lombok.Getter;
 
-@Getter
 public enum Role {
-    USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN"),
-    MANAGER("ROLE_MANAGER");
+    USER("USER"),
+    ADMIN("ADMIN"),
+    MANAGER("MANAGER");
 
     private final String value;
 
     Role(String value) {
         this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getPrefixedValue() {
+        return "ROLE_" + value;
     }
 }
