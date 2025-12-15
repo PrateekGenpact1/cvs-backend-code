4a156176f5d41ae0a8361cf3d75d6f47d5d219ef
Prateek825 <prateeksingh825@gmail.com>
2025-12-15 20:03:43 +0530

    added options preflight for multipart

--- src/main/java/com/cvsnewsletter/config/SecurityConfiguration.java
+++ src/main/java/com/cvsnewsletter/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import com.cvsnewsletter.exception.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -58,6 +59,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(WHITE_LIST_URL).permitAll()
 
                         // Role-based access
